aedb0b9935b0ed344b42393a3a881f5f711574dd
Izzan Silmi Aziz <izzan.abad@gmail.com>
2023-01-26 23:21:10 +0700

    jobs

---
+++ Readme.md
@@ -0,0 +1,46 @@
+# Job Scrapper
+Command Line Based Tool for scrap job/vacancy data from JobStreet website.
+
+Please refer to [Note](#note) if you're facing some issues or want to do other things within this repo
+
+## Tech Stack
+1. Python (3.8.5) with packages installed as listed in `Pipfile` and `Pipfile.lock`
+2. Chromedriver 
+   - Download here: [link](https://sites.google.com/a/chromium.org/chromedriver/downloads) and choose one depends on your **Operating System**
+   - Remember to set your `CHROMEDRIVER_PATH` later at [Setup](#setup)
+3. [BigQuery](https://cloud.google.com/bigquery) as **Data Warehouse** (DWH) to store the scrap result
+
+## Setup
+1. Install `pipenv` first by executing `pip install pipenv` . Ensure that you already have `pip` on your machine
+2. Run `pipenv shell` to create a virtual environment if doesn't exist yet
+3. Install the dependecies by executing `pipenv install` command on the same directory where `Pipfile.lock` located
+4. Create `.env` file at the root directory of this project/repo
+5. Fill `PROJECT_ID` on your `.env` file based on your Google Cloud Platform (GCP) account
+6. Then fill `DATASET_ID` on your `.env` file regarding to what dataset name you want. e.g `blankspace-jobstreet`
+7. Change the `CHROMEDRIVER_PATH` on `config.py` to your chromedriver path after download the file
+8. Setup done! Please go straight to [here](#how-to-use-this-tool-after-doing-setup)
+## How to Use this Tool After Doing Setup?
+Simply run `python main.py` to scrap the data
+
+## Scrapping Flow
+All scrapping processes were did by `scrapper.py`. And to give you more clearance about how this program gets the data by scrapping. Here is the flow:
+1. First, it will go to [jobstreet page](https://www.jobstreet.co.id/)
+2. Then it will type the keyword on the search bar and clicked the Enter
+3. It redirects you to search result page, e.g this URL https://www.jobstreet.co.id/en/job-search/senior-data-engineer-jobs/
+4. Before get the job data, it checks the pagination first whether it has only 1 page or more. It will get the maximum page from the pagination to traverse all the pages later
+5. For each job card:
+   1. It will get the datetime attribute of job posting time then do the calculation using `timeago` library to get the real Job Posting Time (xyz day/hour/minute ago)
+   2. It will perform click action then it will scrap the job data that we needed (after the job details content showing at the right side )
+6. Then, the program will visit the next page and do step 5 until the max page of pagination.
+7. If all pages have been visited on a keyword, the rest of process just repeating the step 1-6 for the next search keyword.
+8. The scrap result then will be formed as Pandas DataFrame
+9. After that, we store the DataFrame to CSV and BigQuery to do further analysis
+
+## Scrapping Output
+This program will automatically produce two files after you run it:
+1. The **logs** will be stored at `/logs` . Thus you don't need to create the folder manually
+2. The **CSV** file will be stored at the same dir named `jobstreet_scrap_result.csv`. If you insist on check the CSV file you may open these file
+
+### **Disclaimer**
+From my test on this program at local machine. It tooks **around 1 hour in total** to complete the scrap process (from getting the data until store to BigQuery) using all keywords that listed at `config.py` .<br>
+Thus, if you want to get the result faster you might just delete/comment some keywords.
---
+++ scrapper.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import os
+import pandas_gbq
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from config import BASE_URL, CHROMEDRIVER_PATH, KEYWORDS, SIBLING_PARENT_XPATH, LOG_DIR, LOG_INFO_PATH, LOG_INFO_FILEMODE, BIGQUERY_TABLE_NAME
+from helpers import time_difference_fmt
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from selenium.webdriver.chrome.options import Options
+options = Options()
+# options.add_argument("--headless")
+options.add_argument("window-size=1920,1080")
+
+from pathlib import Path
+Path(LOG_DIR).mkdir(exist_ok=True)
+
+import logging
+logging.basicConfig(level=logging.INFO, filename=LOG_INFO_PATH, filemode=LOG_INFO_FILEMODE)
+
+class JobScrapper(object):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path=CHROMEDRIVER_PATH)
+        self.list_company_name = []
+        self.list_job_posting_time = []
+        self.list_career_level = []
+        self.list_company_size = []
+        self.list_company_industry = []
+        self.list_company_description = []
+        self.list_employment_type = []
+        self.list_job_function = []
+
+    def begin_scrap(self):
+        for keyword in KEYWORDS:
+            self.driver.get(BASE_URL)
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'searchKeywordsField'))).send_keys(keyword)
+
+            search_element = self.driver.find_element(By.ID, 'searchKeywordsField')
+            search_element.send_keys(Keys.ENTER) #input the keyword to search bar
+
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.ID, 'jobList')))
+
+            pagination_elements = self.driver.find_elements(By.ID, 'pagination')
+
+            if len(pagination_elements) > 0:  # pagination element exist
+                pag_element = pagination_elements[0]
+                option_elements = pag_element.find_elements(
+                    By.XPATH, '//*[@id="pagination"]/option')
+                max_page = int(option_elements[-1].text)
+                self.logger.info(f"{keyword} keyword search results max page: {max_page}")
+
+                base_job_url = self.driver.current_url
+
+                for page in range(1, max_page+1):
+                    self.logger.info(f"Current Page: {base_job_url}{page}")
+                    self.driver.get(f"{base_job_url}{page}")
+                    WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_all_elements_located((By.ID, 'jobList')))
+                    article_elements = self.driver.find_elements(By.XPATH, '//article')
+
+                    for i, article_element in enumerate(article_elements):
+                        time_element = article_element.find_element(By.TAG_NAME, 'time')
+                        job_post_iso_time = time_element.get_attribute("datetime")
+                        job_posted_time = time_difference_fmt(posted_time=job_post_iso_time)
+                        self.list_job_posting_time.append(job_posted_time)
+
+                        job_card_element = self.driver.find_element(By.XPATH, f"//article[@data-automation='job-card-{i}']//div//h1/a")
+                        self.driver.execute_script("arguments[0].scrollIntoView(true)", job_card_element)
+                        self.driver.execute_script("arguments[0].click()", job_card_element)
+                        
+                        WebDriverWait(self.driver, 10).until(
+                            EC.presence_of_element_located((By.XPATH, "//div[@data-automation='splitModeJobDetailsScrollWrapper']"))
+                        )
+                        
+                        self.scrap_jobs_data()
+        self.logger.info('Scrapping process Done!')
+
+        #Put together to Pandas Dataframe then Store to BigQuery
+        df = pd.DataFrame(
+            {
+                'company_name': self.list_company_name,
+                'job_posting_time': self.list_job_posting_time,
+                'career_level': self.list_career_level,
+                'size_of_company': self.list_company_size,
+                'company_industry': self.list_company_industry,
+                'detail_description': self.list_company_description,
+                'employment_type': self.list_employment_type,
+                'job_function': self.list_job_function
+            }
+        )
+        
+        df.to_csv('jobstreet_scrap_result.csv', index=False)
+        self.store_to_bigquery(df)
+
+    def scrap_jobs_data(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//div[@data-automation='detailsTitle']//span"))
+        )
+
+        company_name_element = self.driver.find_element(By.XPATH, "//div[@data-automation='detailsTitle']//span")
+        company_name_text = company_name_element.text
+        self.list_company_name.append(company_name_text)
+        self.logger.info(f"Now scrap company name: {company_name_text}")
+
+        company_description_element = self.driver.find_element(By.XPATH, "//div[@data-automation='jobDescription']")
+        company_description_text = company_description_element.text
+        
+        job_highlight_text = self.get_additional_information(
+            selector_to_check="//div[@data-automation='job-details-job-highlights']", 
+            text_selector="//div[@data-automation='job-details-job-highlights']",
+            text='Job Highlight'
+        )
+        
+        benefit_text = self.get_additional_information(
+            selector_to_check="//span[text() = 'Benefits & Others']",
+            text_selector=SIBLING_PARENT_XPATH,
+            is_sibling=True,
+            text='Benefits'
+        )
+        final_description = f"{company_description_text}\n\n{job_highlight_text}\n\nBenefits & Others:\n{benefit_text}"
+        self.list_company_description.append(final_description)
+            
+        career_level_text = self.get_additional_information(
+            selector_to_check="//span[text() = 'Career Level']",
+            text_selector=SIBLING_PARENT_XPATH,
+            is_sibling=True,
+            text='Career Level'
+        )
+        self.list_career_level.append(career_level_text)
+        
+        job_function_text = self.get_additional_information(
+            selector_to_check="//span[text() = 'Job Specializations']",
+            text_selector=SIBLING_PARENT_XPATH,
+            is_sibling=True,
+            text='Job Function'
+        )
+        self.list_job_function.append(job_function_text)
+
+        employment_type_text = self.get_additional_information(
+            selector_to_check="//span[text() = 'Job Type']",
+            text_selector=SIBLING_PARENT_XPATH,
+            is_sibling=True,
+            text='Employment Type'
+        )
+        self.list_employment_type.append(employment_type_text)
+
+        company_size_text = self.get_additional_information(
+            selector_to_check="//span[text() = 'Company Size']",
+            text_selector=SIBLING_PARENT_XPATH,
+            is_sibling=True,
+            text='Company Size'
+        )
+        self.list_company_size.append(company_size_text)
+        
+        industry_text = self.get_additional_information(
+            selector_to_check="//span[text() = 'Industry']",
+            text_selector=SIBLING_PARENT_XPATH,
+            is_sibling=True,
+            text='Company Industry'
+        )
+        self.list_company_industry.append(industry_text)
+        
+    def get_additional_information(self, selector_to_check, text_selector, text, is_sibling=False):
+        prop_elements = self.driver.find_elements(By.XPATH, selector_to_check)
+        if len(prop_elements) >= 1:
+            if is_sibling is True:
+                sibling_element = prop_elements[-1].find_element(By.XPATH, text_selector)
+                information_text = sibling_element.text
+            else:
+                element = self.driver.find_element(By.XPATH, text_selector)
+                information_text = element.text
+        else:
+            information_text = f'No {text}'
+        
+        return information_text
+
+    def store_to_bigquery(self, df):
+        self.logger.info('Writing to BigQuery...')
+        dataset_id = os.getenv('DATASET_ID')
+        project_id = os.getenv('PROJECT_ID')
+        pandas_gbq.to_gbq(
+            df, f"{dataset_id}.{BIGQUERY_TABLE_NAME}", 
+            project_id=project_id, if_exists="replace"
+        )
+        self.logger.info('SUCCESS writing to BigQuery')
